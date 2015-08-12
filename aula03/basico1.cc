7693e38f3cab46bc60fc705ba3555b7d34bee0e4
Marco Aurelio Souza Mangan <10067320@l1670629.portoalegre.pucrsnet.br>
2015-08-11 21:05:26 -0300

    Aula 03

---
+++ aula03/basico1.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Primeiro programa em C++" << std::endl;
+
+	return 0;
+}
---
+++ aula03/soma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+
+	scanf("%i", &a);
+	scanf("%i", &b);
+
+	printf("%i\n", a + b);
+
+	return 0;
+}
