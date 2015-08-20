fd02bdf1d85a0cec4fe5545e59cbcefbacc63906
Marco Aurelio Souza Mangan <10067320@l1975085.portoalegre.pucrsnet.br>
2015-08-20 20:42:42 -0300

    simplificar fracao

--- aula06/ex02.cc
+++ aula06/ex02.cc
@@ -4,49 +4,93 @@
 using namespace std;
 
 struct Fracao {
-	int numerador; 
-	int denominador;
+    int numerador;
+    int denominador;
 };
 
 Fracao multiplicar(Fracao a, Fracao b);
+Fracao somar(Fracao a, Fracao b);
 void mostrar(Fracao a);
+Fracao simplificar(Fracao f);
 
 int main()
 {
-	Fracao f1, f2, r;
+    Fracao f1, f2, p, s;
 
-	f1.numerador = 12;
-	f1.denominador = 17;
+    f1.numerador = 2;
+    f1.denominador = 1;
 
-	f2.numerador = 13;
-	f2.denominador = 18;
+    f2.numerador = 3;
+    f2.denominador = 1;
 
-	mostrar(f1);
-	mostrar(f2);
+    mostrar(f1);
+    mostrar(f2);
 
-	r = multiplicar(f1, f2);
+    p = multiplicar(f1, f2);
 
-	mostrar(r);
+    mostrar(p);
 
-	return 0;
+    s = somar(f1, f2);
+
+    mostrar(s);
+
+
+    return 0;
 }
 
 Fracao multiplicar(Fracao a, Fracao b)
 {
-	Fracao res; // FIXME: verificar alocação automática
+    Fracao res; // FIXME: verificar alocação automática
 
-	res.numerador = a.numerador * b.numerador;
-	res.denominador = a.denominador * b.denominador;
+    res.numerador = a.numerador * b.numerador;
+    res.denominador = a.denominador * b.denominador;
 
-	return res;
+    mostrar(res);
+    return simplificar(res);
+}
+
+//TODO: completar
+Fracao somar(Fracao a, Fracao b)
+{
+    Fracao res; // FIXME: verificar alocação automática
+
+    res.numerador = res.denominador/a.denominador * a.numerador
+                    + res.denominador/b.denominador * b.numerador;
+    res.denominador = a.denominador * b.denominador;
+
+    mostrar(res);
+    return simplificar(res);
+}
+
+
+Fracao simplificar(Fracao f)
+{
+    // a=>numerador
+    // b=>denominador
+    // enquanto b for diferente de zero:
+    //   tmp = a
+    //   a = b
+    //   b = tmp % b
+    int a = f.numerador;
+    int b = f.denominador;
+    while(b != 0)
+    {
+        int tmp = a;
+        a = b;
+        b = tmp % b;
+    }
+    // MDC esta' no a
+    f.denominador = f.denominador / a;
+    f.numerador   = f.numerador   / a;
+    return f;
 }
 
 void mostrar(Fracao a)
 {
-	cout <<  a.numerador
-	     <<  " / "
-	     <<  a.denominador
-	     <<  endl;
+    cout <<  a.numerador
+         <<  " / "
+         <<  a.denominador
+         <<  endl;
 }
 
 
